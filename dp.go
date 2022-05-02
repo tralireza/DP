20ed81235eeff480b26afb4ac9deb0a54a0d14fd
Alireza <26859725+tralireza@users.noreply.github.com>
2022-05-02 11:23:00 +0100

    Recursion, BackTracking: PowerSet recursive generator
    1863 Sum of All Subset XOR Totals

--- dp.go
+++ dp.go
@@ -6,3 +6,32 @@ func init() {
 	log.SetFlags(0)
 	log.SetPrefix("")
 }
+
+// 1863 Sum of All Subset XOR Totals
+func subsetXORSum(nums []int) int {
+
+	var PowerSet func(int) [][]int
+	PowerSet = func(i int) [][]int {
+		if i == len(nums) {
+			return [][]int{{}}
+		}
+		r := [][]int{}
+		for _, l := range PowerSet(i + 1) {
+			r = append(r, l)
+			r = append(r, append([]int{nums[i]}, l...))
+		}
+		return r
+	}
+
+	tSum := 0
+	for _, l := range PowerSet(0) {
+		if len(l) > 0 {
+			v := l[0]
+			for _, n := range l[1:] {
+				v ^= n
+			}
+			tSum += v
+		}
+	}
+	return tSum
+}
--- dp_test.go
+++ dp_test.go
@@ -5,3 +5,26 @@ import "log"
 func init() {
 	log.Print("> Dynamic Programming")
 }
+
+// 1863 Sum of All Subset XOR Totals
+func Test1863(t *testing.T) {
+	// 2^n Power sets
+	var PowerSet func([]int, int) [][]int
+	PowerSet = func(nums []int, i int) [][]int {
+		if i == len(nums) {
+			return [][]int{{}}
+		}
+
+		r := [][]int{}
+		for _, l := range PowerSet(nums, i+1) {
+			r = append(r, l)
+			r = append(r, append([]int{nums[i]}, l...))
+		}
+		return r
+	}
+	log.Printf("%v -> %v", []int{0, 1, 2, 3}, PowerSet([]int{0, 1, 2, 3}, 0))
+
+	log.Print("6 ?= ", subsetXORSum([]int{1, 3}))
+	log.Print("28 ?= ", subsetXORSum([]int{5, 1, 6}))
+	log.Print("480 ?= ", subsetXORSum([]int{3, 4, 5, 6, 7, 8}))
+}
